97ca44686bb7caa710f3b30a4ab3b1e79185c87a
alexandrelima658 <alexandrec658@gmail.com>
2024-01-11 14:37:29 -0300

    feature: implementing a category update

--- fc3-admin-do-catalogo/domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
+++ fc3-admin-do-catalogo/domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
@@ -61,6 +61,18 @@ public class Category extends AggregateRoot<CategoryID> {
         return this;
     }
 
+    public Category update(final String aName, final String aDescription, final boolean isActive){
+        this.description = aDescription;
+        if(isActive) {
+            activate();
+        }else{
+            deactivate();
+        }
+        this.name = aName;
+        this.updatedAt = Instant.now();
+        return this;
+    }
+
     public CategoryID getId() {
         return id;
     }
