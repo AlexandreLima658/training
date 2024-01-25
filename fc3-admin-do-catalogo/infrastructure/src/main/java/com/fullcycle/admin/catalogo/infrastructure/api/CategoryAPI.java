ec59bb98318960369327dbf9d4bb9c615d83e865
alexandrelima658 <alexandrec658@gmail.com>
2024-01-25 14:38:13 -0300

    feature: Add CORS Configuration

--- fc3-admin-do-catalogo/infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/CategoryAPI.java
+++ fc3-admin-do-catalogo/infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/CategoryAPI.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(value = "categories")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Tag(name = "Categories")
 public interface CategoryAPI {
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
